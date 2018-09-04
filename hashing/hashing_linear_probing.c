c17087c418975e53ac5cd4b0cdf0212d5762fa15
srinidhi <kss682@gmail.com>
2018-09-04 22:58:40 +0530

    Linear probing

---
+++ hashing/hashing_linear_probing.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAX 10
+
+int hash(int number,int arr[],int n)
+{
+int index=number%10;
+while(arr[index]!=0)
+	{
+	index+=1;
+	if(index>n)
+		index=0;
+	}
+return index;
+}
+
+
+
+void main()
+{
+int n,arr[MAX]={0*MAX},number,value,i,index;
+printf("enter no of elements\n");
+scanf("%d",&n);
+printf("\n enter the elements \n");
+for(i=0;i<n;i++)
+	{scanf("%d",&number);
+	index=hash(number,arr,n);
+	arr[index]=number;
+	}
+
+printf("\n________\n");
+for(i=0;i<MAX;i++)
+	{printf("|%2d | %4d |\n",arr[i],i);
+	
+	}
+printf("\n________\n");
+}
+
+	
