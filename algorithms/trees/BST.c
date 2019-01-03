28ef95318f711eab526e07c2b644ef199243ab4f
srinidhi <kss682@gmail.com>
2019-01-03 13:05:22 +0530

    BST

---
+++ algorithms/trees/BST.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+  int data;
+  struct node *right;
+  struct node *left;
+}*root = NULL;
+
+struct node* insert(struct node *tree,int item)
+{
+  if(tree == NULL)
+    {tree = malloc(sizeof(struct node));
+    tree->left = NULL;
+    tree->right = NULL;
+    tree->data = item;
+    }
+  else if(tree->data > item)
+    {
+      tree->left = insert(tree->left,item);
+    }
+  else if(tree->data < item)
+    {
+      tree->right = insert(tree->right,item);
+    }
+  else if(tree->data == item)
+    printf("Duplicate data\n");
+  return tree;
+}
+
+void search(struct node *tree,int item)
+{
+  if(tree == NULL)
+  {
+    printf("No such item\n");
+  }
+  else if(tree->data == item)
+    {
+      printf("Found item\n");
+    }
+  else if(tree->data > item)
+    {
+      search(tree->left,item);
+    }
+  else if(tree->data < item)
+    {
+      search(tree->right,item);
+    }
+}
+
+void preorder(struct node *tree)
+{
+  if(tree == NULL)
+    return;
+  printf("%d\n",tree->data);
+  preorder(tree->left);
+  preorder(tree->right);
+}
+void inorder(struct node *tree)
+{
+  if(tree == NULL)
+    return;
+  preorder(tree->left);
+  printf("%d\n",tree->data);
+  preorder(tree->right);
+}
+void postorder(struct node *tree)
+{
+  if(tree == NULL)
+    return;
+  preorder(tree->left);
+  preorder(tree->right);
+  printf("%d\n",tree->data);
+}
+int main()
+{
+
+  int ch,item;
+  do{
+  printf("\n\tMENU\n\t1)Insert\n\t2)search\n\t3)Preorder\n\t4)inorder\n\t5)postorder\n\tEnter choice");
+  scanf("%d",&ch);
+  switch (ch) {
+    case 1 : printf("Enter the value\n");
+            scanf("%d",&item);
+            root = insert(root,item);
+              break;
+    case 2 : printf("Enter the value\n");
+            scanf("%d",&item);
+              search(root,item);
+              break;
+    case 3 : preorder(root);
+              break;
+    case 4 : inorder(root);
+              break;
+    case 5 : postorder(root);
+              break;
+  }
+}while(ch!=0);
+  return 0;
+}
