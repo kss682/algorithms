cabe51a76edef2d8ebb4ec0c739d428aa2417170
kss682 <kss682@gmail.com>
2020-10-02 18:19:37 +0530

    cleaning up files

--- competitive_coding/codeforce/A/1335A.py
+++
@@ -1,9 +0,0 @@
-# sample https://codeforces.com/problemset/problem/1335/A
-T = int(input())
-while T > 0:
-	N = int(input())
-	if N%2 == 0:
-		print(N//2-1)
-	else:
-		print(N//2) 
-	T -= 1
--- competitive_coding/codeforce/A/4A.py
+++
@@ -1,8 +0,0 @@
-# https://codeforces.com/problemset/problem/4/A
-W = int(input())
-if W == 2:
-	print("NO")
-elif W%2 == 0:
-	print("YES")
-else:
-	print("NO")
